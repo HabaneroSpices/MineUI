be7d513f4f4e355f43c328d9117a3b3728c50114
habanerospices <habanerospices@gmail.com>
2020-09-01 15:11:38 +0200

    Changed some outputs

--- install.sh
+++ install.sh
@@ -2,31 +2,31 @@
 #
 #MINECRAFT_PATH=`cat ./.settings`
 
-echo "This script requires sudo privleges"
+echo "[!] This script requires sudo privleges"
 sudo -v
 
 install_here() {
 
     MINECRAFT_PATH=`cat ./.settings`
-    echo "Setting privledges..."
+    echo "[*] Setting privledges..."
     sudo chmod +x .mcsrv.sh
-    echo "Creating junction for logs..."
+    echo "[*] Logging..."
     sudo ln -s ${MINECRAFT_PATH}logs
 
 }
 
 depend_aptyum() {
-    echo "Please input path to minecraft server"
+    echo "[!] Please input path to minecraft server"
     echo "Example: /path/to/server/"
     read -r -p 'Location: ' SERVER_PATH
-    echo "Creating .settings"
+    echo "[*] Crafting .settings"
     sudo echo ${SERVER_PATH} > .settings
 }
 
 
 depend_aptyum
 install_here
-echo "Done!"
+echo "[+] Done!"
 
 # Obsolete
 #case $1 in
